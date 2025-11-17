a9ff0767a93a00c63862091492373ba4ea6886a0
Dirasir <baicyrovdaniil2007@gmail.com>
2025-11-18 01:35:05 +0300

    create plugin

---
+++ src/extension.ts
@@ -0,0 +1,38 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import { exec } from 'child_process';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('russian-hymn.playHymn', () => {
+        playHymn(context);
+    });
+    context.subscriptions.push(disposable);
+}
+
+function playHymn(context: vscode.ExtensionContext) {
+    const audioPath = path.join(context.extensionPath, 'sounds', 'hymn.mp3');
+    
+    if (!fs.existsSync(audioPath)) {
+        vscode.window.showErrorMessage(`Audio file not found: ${audioPath}`);
+        return;
+    }
+
+    if (process.platform === 'win32') {
+        const command = `powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName presentationCore; $player = New-Object System.Windows.Media.MediaPlayer; $player.Open('${audioPath.replace(/\\/g, '\\\\')}'); $player.Play(); Start-Sleep -Seconds 300"`;
+        
+        exec(command, (error) => {
+            if (error) {
+                vscode.window.showErrorMessage('Error playing audio: ' + error.message);
+                vscode.env.openExternal(vscode.Uri.file(audioPath));
+            } else {
+                vscode.window.showInformationMessage('🎵 Russian Hymn playing in background!');
+            }
+        });
+    } else {
+        vscode.env.openExternal(vscode.Uri.file(audioPath));
+        vscode.window.showInformationMessage('🎵 Opening Russian Hymn!');
+    }
+}
+
+export function deactivate() {}
